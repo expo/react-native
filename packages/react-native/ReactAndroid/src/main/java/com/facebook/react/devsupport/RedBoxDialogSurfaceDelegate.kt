6a596f4cccba4ac30263723635cbc9a51cc49486
Ben Roth <spongebucket@gmail.com>
2017-03-22 14:47:03 -0700

    Patch DevTools classes - Remove swizzling from dev tools - Remove RCT_EXPORT_MODULE from RCTDevMenu
    Do not use RCT_EXPORT_MODULE in RCTDevMenu
    
    Because this class swizzles a system method inside `initialize`, all the versions of RCTDevMenu conflict with each other when we use RCT_EXPORT_MODULE because they are all referenced immediately at runtime.
    
    [iOS] Remove side effects from DevTools (swizzling, module exporting)
    
    - Remove swizzling from dev tools
    - Remove RCT_EXPORT_MODULE from RCTDevMenu
    
    Do not use RCT_EXPORT_MODULE in RCTDevMenu
    
    Because this class swizzles a system method inside `initialize`, all the versions of RCTDevMenu conflict with each other when we use RCT_EXPORT_MODULE because they are all referenced immediately at runtime.
    
    Co-Authored-By: Kudo Chien <kudo@expo.dev>
    * Update patch to fit newer RCTKeyCommands.m
    
    reapply 65fdcab
    
    [android] allow overriding DevServerPort and InspectorProxyPort
    
    [0.80] Change type from String? -> String (#40)

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/devsupport/RedBoxDialogSurfaceDelegate.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/devsupport/RedBoxDialogSurfaceDelegate.kt
@@ -97,7 +97,7 @@ internal class RedBoxDialogSurfaceDelegate(private val devSupportManager: DevSup
                     return true
                   }
                   if (doubleTapReloadRecognizer.didDoubleTapR(keyCode, currentFocus)) {
-                    devSupportManager.handleReloadJS()
+                    devSupportManager.reloadExpoApp()
                   }
                   return super.onKeyUp(keyCode, event)
                 }
