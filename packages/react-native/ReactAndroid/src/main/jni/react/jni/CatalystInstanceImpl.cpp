9751c3fa3027fc2ecc3bcc0095758c25c410ef57
James Ide <ide@expo.io>
2018-02-21 15:51:39 -0800

    [android] Add method to load a script from string instead of file
    Loading directly from a string instead of a file provides more flexibility for bundle sources other than disk.

--- packages/react-native/ReactAndroid/src/main/jni/react/jni/CatalystInstanceImpl.cpp
+++ packages/react-native/ReactAndroid/src/main/jni/react/jni/CatalystInstanceImpl.cpp
@@ -105,6 +105,8 @@ void CatalystInstanceImpl::registerNatives() {
       makeNativeMethod(
           "jniLoadScriptFromFile", CatalystInstanceImpl::jniLoadScriptFromFile),
       makeNativeMethod(
+          "jniLoadScriptFromString", CatalystInstanceImpl::jniLoadScriptFromString),
+      makeNativeMethod(
           "jniCallJSFunction", CatalystInstanceImpl::jniCallJSFunction),
       makeNativeMethod(
           "jniCallJSCallback", CatalystInstanceImpl::jniCallJSCallback),
@@ -291,6 +293,14 @@ void CatalystInstanceImpl::jniLoadScriptFromFile(
           std::move(script), sourceURL, loadSynchronously);
     }
   }
+}
+
+void CatalystInstanceImpl::jniLoadScriptFromString(
+    const std::string& scriptStdString,
+    const std::string& sourceURL,
+    bool loadSynchronously) {
+  std::unique_ptr<const JSBigStdString> script = std::make_unique<const JSBigStdString>(scriptStdString);
+  instance_->loadScriptFromString(std::move(script), sourceURL, loadSynchronously);
 }
 
 void CatalystInstanceImpl::jniCallJSFunction(
