71b804db3f0df5a2fd706475636ad972bbac0dc1
James Ide <ide@expo.io>
2018-02-21 15:51:39 -0800

    [android] Add method to load a script from string instead of file
    Loading directly from a string instead of a file provides more flexibility for bundle sources other than disk.

--- packages/react-native/ReactAndroid/src/main/jni/react/jni/CatalystInstanceImpl.h
+++ packages/react-native/ReactAndroid/src/main/jni/react/jni/CatalystInstanceImpl.h
@@ -91,6 +91,10 @@ class CatalystInstanceImpl : public jni::HybridClass<CatalystInstanceImpl> {
       const std::string &fileName,
       const std::string &sourceURL,
       bool loadSynchronously);
+  void jniLoadScriptFromString(
+      const std::string& fileName,
+      const std::string& sourceURL,
+      bool loadSynchronously);
   void jniCallJSFunction(
       std::string module,
       std::string method,
