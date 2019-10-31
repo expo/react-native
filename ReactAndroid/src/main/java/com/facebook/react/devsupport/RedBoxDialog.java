9b9c60c064c473b4e1b6d4ff2dfa5223975d578b
Stanisław Chmiela <sjchmiela@gmail.com>
2019-10-31 14:27:21 +0100

    [ReactAndroid] Prefix all resources with 'reactandroid_'

--- ReactAndroid/src/main/java/com/facebook/react/devsupport/RedBoxDialog.java
+++ ReactAndroid/src/main/java/com/facebook/react/devsupport/RedBoxDialog.java
@@ -167,7 +167,7 @@ import org.json.JSONObject;
                 ? (TextView) convertView
                 : (TextView)
                     LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.redbox_item_title, parent, false);
+                        .inflate(R.layout.reactandroid_redbox_item_title, parent, false);
         // Remove ANSI color codes from the title
         title.setText(mTitle.replaceAll("\\x1b\\[[0-9;]*m", ""));
         return title;
@@ -175,7 +175,7 @@ import org.json.JSONObject;
         if (convertView == null) {
           convertView =
               LayoutInflater.from(parent.getContext())
-                  .inflate(R.layout.redbox_item_frame, parent, false);
+                  .inflate(R.layout.reactandroid_redbox_item_frame, parent, false);
           convertView.setTag(new FrameViewHolder(convertView));
         }
         StackFrame frame = mStack[position - 1];
@@ -235,7 +235,7 @@ import org.json.JSONObject;
 
     requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-    setContentView(R.layout.redbox_view);
+    setContentView(R.layout.reactandroid_redbox_view);
 
     mDevSupportManager = devSupportManager;
     mDoubleTapReloadRecognizer = new DoubleTapReloadRecognizer();
