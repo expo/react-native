704c4a4d5dcdbeed8ad1eb19ffef7361193540ed
Brent Vatne <brentvatne@gmail.com>
2019-11-07 11:11:28 -0800

    [android] Always reload from manifest https://github.com/expo/expo/pull/6134

--- ReactAndroid/src/main/java/com/facebook/react/devsupport/DevInternalSettings.java
+++ ReactAndroid/src/main/java/com/facebook/react/devsupport/DevInternalSettings.java
@@ -21,159 +21,169 @@ import com.facebook.react.packagerconnection.PackagerConnectionSettings;
  * this class implements an external interface {@link DeveloperSettings}.
  */
 @VisibleForTesting
-public class DevInternalSettings
-    implements DeveloperSettings, SharedPreferences.OnSharedPreferenceChangeListener {
+public class DevInternalSettings implements DeveloperSettings, SharedPreferences.OnSharedPreferenceChangeListener {
 
-  private static final String PREFS_FPS_DEBUG_KEY = "fps_debug";
-  private static final String PREFS_JS_DEV_MODE_DEBUG_KEY = "js_dev_mode_debug";
-  private static final String PREFS_JS_MINIFY_DEBUG_KEY = "js_minify_debug";
-  private static final String PREFS_JS_BUNDLE_DELTAS_KEY = "js_bundle_deltas";
-  private static final String PREFS_JS_BUNDLE_DELTAS_CPP_KEY = "js_bundle_deltas_cpp";
-  private static final String PREFS_ANIMATIONS_DEBUG_KEY = "animations_debug";
-  // This option is no longer exposed in the dev menu UI.
-  // It was renamed in D15958697 so it doesn't get stuck with no way to turn it off:
-  private static final String PREFS_RELOAD_ON_JS_CHANGE_KEY = "reload_on_js_change_LEGACY";
-  private static final String PREFS_INSPECTOR_DEBUG_KEY = "inspector_debug";
-  private static final String PREFS_HOT_MODULE_REPLACEMENT_KEY = "hot_module_replacement";
-  private static final String PREFS_REMOTE_JS_DEBUG_KEY = "remote_js_debug";
-  private static final String PREFS_START_SAMPLING_PROFILER_ON_INIT =
-      "start_sampling_profiler_on_init";
+    public static String PREFS_FPS_DEBUG_KEY = "fps_debug";
 
-  private final SharedPreferences mPreferences;
-  private final Listener mListener;
-  private final PackagerConnectionSettings mPackagerConnectionSettings;
-  private final boolean mSupportsNativeDeltaClients;
+    public static String PREFS_JS_DEV_MODE_DEBUG_KEY = "js_dev_mode_debug";
 
-  public static DevInternalSettings withoutNativeDeltaClient(
-      Context applicationContext, Listener listener) {
-    return new DevInternalSettings(applicationContext, listener, false);
-  }
+    public static String PREFS_JS_MINIFY_DEBUG_KEY = "js_minify_debug";
 
-  public DevInternalSettings(Context applicationContext, Listener listener) {
-    this(applicationContext, listener, true);
-  }
+    public static String PREFS_JS_BUNDLE_DELTAS_KEY = "js_bundle_deltas";
 
-  private DevInternalSettings(
-      Context applicationContext, Listener listener, boolean supportsNativeDeltaClients) {
-    mListener = listener;
-    mPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext);
-    mPreferences.registerOnSharedPreferenceChangeListener(this);
-    mPackagerConnectionSettings = new PackagerConnectionSettings(applicationContext);
-    mSupportsNativeDeltaClients = supportsNativeDeltaClients;
-  }
+    public static String PREFS_JS_BUNDLE_DELTAS_CPP_KEY = "js_bundle_deltas_cpp";
 
-  public PackagerConnectionSettings getPackagerConnectionSettings() {
-    return mPackagerConnectionSettings;
-  }
+    public static String PREFS_ANIMATIONS_DEBUG_KEY = "animations_debug";
 
-  @Override
-  public boolean isFpsDebugEnabled() {
-    return mPreferences.getBoolean(PREFS_FPS_DEBUG_KEY, false);
-  }
+    // This option is no longer exposed in the dev menu UI.
+    // It was renamed in D15958697 so it doesn't get stuck with no way to turn it off:
+    public static String PREFS_RELOAD_ON_JS_CHANGE_KEY = "reload_on_js_change_LEGACY";
 
-  public void setFpsDebugEnabled(boolean enabled) {
-    mPreferences.edit().putBoolean(PREFS_FPS_DEBUG_KEY, enabled).apply();
-  }
+    public static String PREFS_INSPECTOR_DEBUG_KEY = "inspector_debug";
 
-  @Override
-  public boolean isAnimationFpsDebugEnabled() {
-    return mPreferences.getBoolean(PREFS_ANIMATIONS_DEBUG_KEY, false);
-  }
+    public static String PREFS_HOT_MODULE_REPLACEMENT_KEY = "hot_module_replacement";
 
-  @Override
-  public boolean isJSDevModeEnabled() {
-    return mPreferences.getBoolean(PREFS_JS_DEV_MODE_DEBUG_KEY, true);
-  }
+    public static String PREFS_REMOTE_JS_DEBUG_KEY = "remote_js_debug";
 
-  public void setJSDevModeEnabled(boolean value) {
-    mPreferences.edit().putBoolean(PREFS_JS_DEV_MODE_DEBUG_KEY, value).apply();
-  }
+    public static String PREFS_START_SAMPLING_PROFILER_ON_INIT = "start_sampling_profiler_on_init";
 
-  @Override
-  public boolean isJSMinifyEnabled() {
-    return mPreferences.getBoolean(PREFS_JS_MINIFY_DEBUG_KEY, false);
-  }
+    public final SharedPreferences mPreferences;
 
-  public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-    if (mListener != null) {
-      if (PREFS_FPS_DEBUG_KEY.equals(key)
-          || PREFS_RELOAD_ON_JS_CHANGE_KEY.equals(key)
-          || PREFS_JS_DEV_MODE_DEBUG_KEY.equals(key)
-          || PREFS_JS_BUNDLE_DELTAS_KEY.equals(key)
-          || PREFS_JS_BUNDLE_DELTAS_CPP_KEY.equals(key)
-          || PREFS_START_SAMPLING_PROFILER_ON_INIT.equals(key)
-          || PREFS_JS_MINIFY_DEBUG_KEY.equals(key)) {
-        mListener.onInternalSettingsChanged();
-      }
+    public final Listener mListener;
+
+    public final PackagerConnectionSettings mPackagerConnectionSettings;
+
+    public final boolean mSupportsNativeDeltaClients;
+
+    public static DevInternalSettings withoutNativeDeltaClient(Context applicationContext, Listener listener) {
+        return new DevInternalSettings(applicationContext, listener, false);
     }
-  }
 
-  public boolean isHotModuleReplacementEnabled() {
-    return mPreferences.getBoolean(PREFS_HOT_MODULE_REPLACEMENT_KEY, true);
-  }
+    public DevInternalSettings(Context applicationContext, Listener listener) {
+        this(applicationContext, listener, true);
+    }
 
-  public void setHotModuleReplacementEnabled(boolean enabled) {
-    mPreferences.edit().putBoolean(PREFS_HOT_MODULE_REPLACEMENT_KEY, enabled).apply();
-  }
+    private DevInternalSettings(Context applicationContext, Listener listener, boolean supportsNativeDeltaClients) {
+        mListener = listener;
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext);
+        mPreferences.registerOnSharedPreferenceChangeListener(this);
+        mPackagerConnectionSettings = new PackagerConnectionSettings(applicationContext);
+        mSupportsNativeDeltaClients = supportsNativeDeltaClients;
+    }
 
-  public boolean isReloadOnJSChangeEnabled() {
-    return mPreferences.getBoolean(PREFS_RELOAD_ON_JS_CHANGE_KEY, false);
-  }
+    public PackagerConnectionSettings getPackagerConnectionSettings() {
+        return mPackagerConnectionSettings;
+    }
 
-  public void setReloadOnJSChangeEnabled(boolean enabled) {
-    mPreferences.edit().putBoolean(PREFS_RELOAD_ON_JS_CHANGE_KEY, enabled).apply();
-  }
+    @Override
+    public boolean isFpsDebugEnabled() {
+        return mPreferences.getBoolean(PREFS_FPS_DEBUG_KEY, false);
+    }
 
-  public boolean isElementInspectorEnabled() {
-    return mPreferences.getBoolean(PREFS_INSPECTOR_DEBUG_KEY, false);
-  }
+    public void setFpsDebugEnabled(boolean enabled) {
+        mPreferences.edit().putBoolean(PREFS_FPS_DEBUG_KEY, enabled).apply();
+    }
 
-  public void setElementInspectorEnabled(boolean enabled) {
-    mPreferences.edit().putBoolean(PREFS_INSPECTOR_DEBUG_KEY, enabled).apply();
-  }
+    @Override
+    public boolean isAnimationFpsDebugEnabled() {
+        return mPreferences.getBoolean(PREFS_ANIMATIONS_DEBUG_KEY, false);
+    }
 
-  @SuppressLint("SharedPreferencesUse")
-  public boolean isBundleDeltasEnabled() {
-    return mPreferences.getBoolean(PREFS_JS_BUNDLE_DELTAS_KEY, false);
-  }
+    @Override
+    public boolean isJSDevModeEnabled() {
+        return mPreferences.getBoolean(PREFS_JS_DEV_MODE_DEBUG_KEY, true);
+    }
 
-  @SuppressLint("SharedPreferencesUse")
-  public void setBundleDeltasEnabled(boolean enabled) {
-    mPreferences.edit().putBoolean(PREFS_JS_BUNDLE_DELTAS_KEY, enabled).apply();
-  }
+    public void setJSDevModeEnabled(boolean value) {
+        mPreferences.edit().putBoolean(PREFS_JS_DEV_MODE_DEBUG_KEY, value).apply();
+    }
 
-  @SuppressLint("SharedPreferencesUse")
-  public boolean isBundleDeltasCppEnabled() {
-    return mSupportsNativeDeltaClients
-        && mPreferences.getBoolean(PREFS_JS_BUNDLE_DELTAS_CPP_KEY, false);
-  }
+    @Override
+    public boolean isJSMinifyEnabled() {
+        return mPreferences.getBoolean(PREFS_JS_MINIFY_DEBUG_KEY, false);
+    }
 
-  @SuppressLint("SharedPreferencesUse")
-  public void setBundleDeltasCppEnabled(boolean enabled) {
-    mPreferences.edit().putBoolean(PREFS_JS_BUNDLE_DELTAS_CPP_KEY, enabled).apply();
-  }
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (mListener != null) {
+            if (PREFS_FPS_DEBUG_KEY.equals(key) || PREFS_RELOAD_ON_JS_CHANGE_KEY.equals(key) || PREFS_JS_DEV_MODE_DEBUG_KEY.equals(key) || PREFS_JS_BUNDLE_DELTAS_KEY.equals(key) || PREFS_JS_BUNDLE_DELTAS_CPP_KEY.equals(key) || PREFS_START_SAMPLING_PROFILER_ON_INIT.equals(key) || PREFS_JS_MINIFY_DEBUG_KEY.equals(key)) {
+                mListener.onInternalSettingsChanged();
+            }
+        }
+    }
 
-  @Override
-  public boolean isNuclideJSDebugEnabled() {
-    return ReactBuildConfig.IS_INTERNAL_BUILD && ReactBuildConfig.DEBUG;
-  }
+    public boolean isHotModuleReplacementEnabled() {
+        return mPreferences.getBoolean(PREFS_HOT_MODULE_REPLACEMENT_KEY, true);
+    }
 
-  @Override
-  public boolean isRemoteJSDebugEnabled() {
-    return mPreferences.getBoolean(PREFS_REMOTE_JS_DEBUG_KEY, false);
-  }
+    public void setHotModuleReplacementEnabled(boolean enabled) {
+        mPreferences.edit().putBoolean(PREFS_HOT_MODULE_REPLACEMENT_KEY, enabled).apply();
+    }
 
-  @Override
-  public void setRemoteJSDebugEnabled(boolean remoteJSDebugEnabled) {
-    mPreferences.edit().putBoolean(PREFS_REMOTE_JS_DEBUG_KEY, remoteJSDebugEnabled).apply();
-  }
+    public boolean isReloadOnJSChangeEnabled() {
+      // NOTE(brentvatne): This is not possible to enable/disable so we should always disable it for
+      // now. I managed to get into a state where fast refresh wouldn't work because live reload
+      // would kick in every time and there was no way to turn it off from the dev menu.
+        return false;
+      // return mPreferences.getBoolean(PREFS_RELOAD_ON_JS_CHANGE_KEY, true);
+    }
 
-  @Override
-  public boolean isStartSamplingProfilerOnInit() {
-    return mPreferences.getBoolean(PREFS_START_SAMPLING_PROFILER_ON_INIT, false);
-  }
+    public void setReloadOnJSChangeEnabled(boolean enabled) {
+      // NOTE(brentvatne): We don't need to do anything here because this option is always false
+      // mPreferences.edit().putBoolean(PREFS_RELOAD_ON_JS_CHANGE_KEY, enabled).apply();
+    }
 
-  public interface Listener {
-    void onInternalSettingsChanged();
-  }
+    public boolean isElementInspectorEnabled() {
+        return mPreferences.getBoolean(PREFS_INSPECTOR_DEBUG_KEY, false);
+    }
+
+    public void setElementInspectorEnabled(boolean enabled) {
+        mPreferences.edit().putBoolean(PREFS_INSPECTOR_DEBUG_KEY, enabled).apply();
+    }
+
+    @SuppressLint("SharedPreferencesUse")
+    public boolean isBundleDeltasEnabled() {
+        return mPreferences.getBoolean(PREFS_JS_BUNDLE_DELTAS_KEY, false);
+    }
+
+    @SuppressLint("SharedPreferencesUse")
+    public void setBundleDeltasEnabled(boolean enabled) {
+        mPreferences.edit().putBoolean(PREFS_JS_BUNDLE_DELTAS_KEY, enabled).apply();
+    }
+
+    @SuppressLint("SharedPreferencesUse")
+    public boolean isBundleDeltasCppEnabled() {
+        return mSupportsNativeDeltaClients && mPreferences.getBoolean(PREFS_JS_BUNDLE_DELTAS_CPP_KEY, false);
+    }
+
+    @SuppressLint("SharedPreferencesUse")
+    public void setBundleDeltasCppEnabled(boolean enabled) {
+        mPreferences.edit().putBoolean(PREFS_JS_BUNDLE_DELTAS_CPP_KEY, enabled).apply();
+    }
+
+    @Override
+    public boolean isNuclideJSDebugEnabled() {
+        return ReactBuildConfig.IS_INTERNAL_BUILD && ReactBuildConfig.DEBUG;
+    }
+
+    @Override
+    public boolean isRemoteJSDebugEnabled() {
+        return mPreferences.getBoolean(PREFS_REMOTE_JS_DEBUG_KEY, false);
+    }
+
+    @Override
+    public void setRemoteJSDebugEnabled(boolean remoteJSDebugEnabled) {
+        mPreferences.edit().putBoolean(PREFS_REMOTE_JS_DEBUG_KEY, remoteJSDebugEnabled).apply();
+    }
+
+    @Override
+    public boolean isStartSamplingProfilerOnInit() {
+        return mPreferences.getBoolean(PREFS_START_SAMPLING_PROFILER_ON_INIT, false);
+    }
+
+    public interface Listener {
+
+        void onInternalSettingsChanged();
+    }
+
+public int exponentActivityId = -1;
 }
