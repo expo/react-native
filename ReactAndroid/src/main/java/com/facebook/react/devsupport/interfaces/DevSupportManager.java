ec04b37b6805d3bd745ab47e10d108ec8d1aacba
Stanisław Chmiela <sjchmiela@gmail.com>
2020-05-14 12:15:17 +0200

    [android] Always reload from manifest expo/expo#6134 honoring bare-expo and :tools:execute
    Squashed three commits:
    - https://github.com/expo/react-native/commit/d8f3fbef53dc53f94dc08b7476b0fc5f15b7771b
    - https://github.com/expo/react-native/commit/ae5e396b37a0d21b2dfe578502f738c9f537f1d1
    - https://github.com/expo/react-native/commit/221762012097c1dcda25762c774eafc39962b182
    
    The first one introduced breaking changes for bare workflow in our React Native fork,
    the second one fixed them by integrating with ReactAndroidCodeTransformer used when we
    import the code into Expo Client, the third one added usage for `reloadExpoApp`
    to debugger UI.
    
    Co-Authored-By: brentvatne <brentvatne@gmail.com>
    Co-Authored-By: esamelson <eric@expo.io>
    Co-Authored-By: Kudo Chien <kudo@expo.dev>

--- ReactAndroid/src/main/java/com/facebook/react/devsupport/interfaces/DevSupportManager.java
+++ ReactAndroid/src/main/java/com/facebook/react/devsupport/interfaces/DevSupportManager.java
@@ -72,6 +72,8 @@ public interface DevSupportManager extends NativeModuleCallExceptionHandler {
 
   void handleReloadJS();
 
+  void reloadExpoApp();
+
   void reloadJSFromServer(final String bundleURL);
 
   void reloadJSFromServer(final String bundleURL, final BundleLoadCallback callback);
