62d75bfd435698b5df136903be6e64321376b5e9
Kudo Chien <kudo@expo.dev>
2024-01-23 18:20:13 +0800

    Fix appearance changed into wrong activity (#34)
    the original `AppCompatDelegate.setDefaultNightMode()` is a global setup. when changing the night mode, it will actually call HomeActivity's `onConfigurationChanged`. this pr tries to set night mode to current activity by using the `setLocalNightMode()`

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/appearance/AppearanceModule.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/modules/appearance/AppearanceModule.kt
@@ -65,12 +65,21 @@ constructor(
   }
 
   public override fun setColorScheme(style: String) {
+    var nightMode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+    if (style == "dark") {
+      nightMode = AppCompatDelegate.MODE_NIGHT_YES
+    } else if (style == "light") {
+      nightMode = AppCompatDelegate.MODE_NIGHT_NO
+    } else if (style == "unspecified") {
+      nightMode = AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+    }
+
     UiThreadUtil.runOnUiThread {
-      when (style) {
-        "dark" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        "light" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        "unspecified" ->
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+      val activity = currentActivity
+      if (activity is AppCompatActivity) {
+        activity.delegate.localNightMode = nightMode
+      } else {
+        AppCompatDelegate.setDefaultNightMode(nightMode)
       }
     }
   }
