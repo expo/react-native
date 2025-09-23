b58a22bfbe3a6e8f044d6a0ca85d98ffafea322b
Gabriel Donadel <donadeldev@gmail.com>
2025-09-23 11:00:58 -0300

    et update-rn

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/BaseReactPackage.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/BaseReactPackage.kt
@@ -49,7 +49,7 @@ public abstract class BaseReactPackage : ReactPackage {
    * @param reactContext [ReactApplicationContext]
    * @return
    */
-  internal fun getNativeModuleIterator(
+  public fun getNativeModuleIterator(
       reactContext: ReactApplicationContext
   ): Iterable<ModuleHolder> {
     val entrySet = getReactModuleInfoProvider().getReactModuleInfos().entries
--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/devsupport/BridgelessDevSupportManager.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/devsupport/BridgelessDevSupportManager.kt
@@ -23,11 +23,11 @@ import com.facebook.react.packagerconnection.RequestHandler
  * [DevSupportManagerBase] with some additional, more flexible APIs for asynchronously loading the
  * JS bundle.
  *
- * @constructor The primary constructor mirrors the same constructor we have for
+ * @public constructor The primary public constructor mirrors the same public constructor we have for
  *   [BridgeDevSupportManager] and
  *     * is kept for backward compatibility.
  */
-internal class BridgelessDevSupportManager(
+public class BridgelessDevSupportManager(
     applicationContext: Context,
     reactInstanceManagerHelper: ReactInstanceDevHelper,
     packagerPathForJSBundleName: String?,
@@ -54,7 +54,7 @@ internal class BridgelessDevSupportManager(
         pausedInDebuggerOverlayManager,
     ) {
 
-  constructor(
+  public constructor(
       context: Context,
       reactInstanceManagerHelper: ReactInstanceDevHelper,
       packagerPathForJSBundleName: String?,
@@ -82,7 +82,7 @@ internal class BridgelessDevSupportManager(
     reactInstanceDevHelper.reload("BridgelessDevSupportManager.handleReloadJS()")
   }
 
-  fun tracingState(): TracingState {
+  public fun tracingState(): TracingState {
     return TracingState.DISABLED
   }
 }
