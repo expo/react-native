bdd72783686af01705912c51ccb05adbe23eacef
Alan Hughes <alan@expo.io>
2026-08-18 16:51:52 +0100

    Guard the dev settings activity behind a debug check
    DevSettingsActivity is only declared in ReactAndroid's debug manifest, so offering this option in a
    release build that still has dev support would fail to resolve the activity.

--- packages/react-native/ReactAndroid/src/main/java/com/facebook/react/devsupport/DevSupportManagerBase.kt
+++ packages/react-native/ReactAndroid/src/main/java/com/facebook/react/devsupport/DevSupportManagerBase.kt
@@ -47,6 +47,7 @@ import com.facebook.react.common.ReactConstants
 import com.facebook.react.common.ShakeDetector
 import com.facebook.react.common.SurfaceDelegate
 import com.facebook.react.common.SurfaceDelegateFactory
+import com.facebook.react.common.build.ReactBuildConfig
 import com.facebook.react.devsupport.DebugOverlayController.Companion.requestPermission
 import com.facebook.react.devsupport.DevServerHelper.PackagerCommandListener
 import com.facebook.react.devsupport.InspectorFlags.getFuseboxEnabled
@@ -517,10 +518,14 @@ public abstract class DevSupportManagerBase(
         }
         devSettings.isFpsDebugEnabled = !devSettings.isFpsDebugEnabled
       }
-      options[applicationContext.getString(R.string.catalyst_settings)] = DevOptionHandler {
-        val intent = Intent(applicationContext, DevSettingsActivity::class.java)
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        applicationContext.startActivity(intent)
+      // DevSettingsActivity is only declared in ReactAndroid's debug manifest, so offering this
+      // option in a release build that still has dev support would fail to resolve the activity.
+      if (ReactBuildConfig.DEBUG) {
+        options[applicationContext.getString(R.string.catalyst_settings)] = DevOptionHandler {
+          val intent = Intent(applicationContext, DevSettingsActivity::class.java)
+          intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+          applicationContext.startActivity(intent)
+        }
       }
     }
 
