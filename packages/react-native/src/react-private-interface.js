b4e7d025c0dee9e81db392a7daa63e3f55b35f29
Rubén Norte <rubennorte@meta.com>
2026-09-09 04:48:55 -0700

    Export the native Fabric UI manager for React (#58398)
    Summary:
    Pull Request resolved: https://github.com/react/react-native/pull/58398
    
    Expose the native Fabric UI manager through `react-native/react-private-interface` so the React renderer can consume it without reading the global directly.
    
    Changelog: [Internal]
    
    Reviewed By: cortinico
    
    Differential Revision: D119165837
    
    fbshipit-source-id: a41860ef4962c9a71b8052db29f7ff86529db18e
    (cherry picked from commit 73a76ddced2088d925809432fd6b0503f239dbfe)

--- packages/react-native/src/react-private-interface.js
+++ packages/react-native/src/react-private-interface.js
@@ -29,6 +29,7 @@ import typeof ExceptionsManager from '../Libraries/Core/ExceptionsManager';
 import typeof RawEventEmitter from '../Libraries/Core/RawEventEmitter';
 import typeof ReactFiberErrorDialog from '../Libraries/Core/ReactFiberErrorDialog';
 import typeof RCTEventEmitter from '../Libraries/EventEmitter/RCTEventEmitter';
+import type {Spec as FabricUIManager} from '../Libraries/ReactNative/FabricUIManager';
 import typeof {
   createPublicInstance,
   createPublicRootInstance,
@@ -77,6 +78,9 @@ module.exports = {
   },
   get UIManager(): UIManager {
     return require('../Libraries/ReactNative/UIManager').default;
+  },
+  get fabricUIManager(): ?FabricUIManager {
+    return require('../Libraries/ReactNative/FabricUIManager').getFabricUIManager();
   },
   // TODO: Remove when React has migrated to `createAttributePayload` and `diffAttributePayloads`
   get deepDiffer(): deepDiffer {
